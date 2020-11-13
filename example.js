af56135edb55f8e3ad80172f74ea5676b41bf165
karl work <karl.swatman@gmail.com>
2020-11-13 17:08:46 +0000

    change

---
+++ example.js
@@ -0,0 +1,162 @@
+var columnDefs = [
+  {
+    field: 'athlete',
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+    },
+  },
+  {
+    field: 'age',
+    maxWidth: 100,
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      buttons: ['apply', 'reset'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: 'country',
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      buttons: ['clear', 'apply'],
+    },
+  },
+  {
+    field: 'year',
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      buttons: ['apply', 'cancel'],
+      closeOnApply: true,
+    },
+    maxWidth: 100,
+  },
+  { field: 'sport' },
+  { field: 'gold', filter: 'agNumberColumnFilter' },
+  { field: 'silver', filter: 'agNumberColumnFilter' },
+  { field: 'bronze', filter: 'agNumberColumnFilter' },
+  { field: 'total', filter: 'agNumberColumnFilter' },
+];
+
+var gridOptions = {
+  columnDefs: columnDefs,
+  defaultColDef: {
+    flex: 1,
+    minWidth: 150,
+    filter: true,
+  },
+  onFilterChanged: function (e) {
+    console.log('onFilterChanged', e);
+    console.log('gridApi.getFilterModel() =>', e.api.getFilterModel());
+  },
+  onFilterModified: function (e) {
+    console.log('onFilterModified', e);
+    console.log('filterInstance.getModel() =>', e.filterInstance.getModel());
+    console.log(
+      'filterInstance.getModelFromUi() =>',
+      e.filterInstance.getModelFromUi()
+    );
+  },
+  
+  
+};
+
+// XMLHttpRequest in promise format
+function makeRequest(method, url, success, error) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url, true);
+    httpRequest.responseType = "arraybuffer";
+
+    httpRequest.open(method, url);
+    httpRequest.onload = function () {
+        success(httpRequest.response);
+    };
+    httpRequest.onerror = function () {
+        error(httpRequest.response);
+    };
+    httpRequest.send();
+}
+
+// read the raw data and convert it to a XLSX workbook
+function convertDataToWorkbook(data) {
+    /* convert data to binary string */
+    var data = new Uint8Array(data);
+    var arr = new Array();
+
+    for (var i = 0; i !== data.length; ++i) {
+        arr[i] = String.fromCharCode(data[i]);
+    }
+
+    var bstr = arr.join("");
+
+    return XLSX.read(bstr, {type: "binary"});
+}
+
+// pull out the values we're after, converting it into an array of rowData
+
+function populateGrid(workbook) {
+    // our data is in the first sheet
+    var firstSheetName = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[firstSheetName];
+
+    // we expect the following columns to be present
+    var columns = {
+        'A': 'athlete',
+        'B': 'age',
+        'C': 'country',
+        'D': 'year',
+        'E': 'date',
+        'F': 'sport',
+        'G': 'gold',
+        'H': 'silver',
+        'I': 'bronze',
+        'J': 'total'
+    };
+
+    var rowData = [];
+
+    // start at the 2nd row - the first row are the headers
+    var rowIndex = 2;
+
+    // iterate over the worksheet pulling out the columns we're expecting
+    while (worksheet['A' + rowIndex]) {
+        var row = {};
+        Object.keys(columns).forEach(function(column) {
+            row[columns[column]] = worksheet[column + rowIndex].w;
+        });
+
+        rowData.push(row);
+
+        rowIndex++;
+    }
+
+    // finally, set the imported rowData into the grid
+    gridOptions.api.setRowData(rowData);
+}
+
+function importExcel() {
+    makeRequest('GET',
+        'https://arlgservices.co.uk/wp-content/export.xlsx',
+        // success
+        function (data) {
+            var workbook = convertDataToWorkbook(data);
+
+            populateGrid(workbook);
+        },
+        // error
+        function (error) {
+            throw error;
+        }
+    );
+}
+
+// wait for the document to be loaded, otherwise
+// ag-Grid will not find the div in the document.
+document.addEventListener("DOMContentLoaded", function () {
+
+    // lookup the container we want the Grid to use
+    var eGridDiv = document.querySelector('#myGrid');
+
+    // create the grid passing in the div to use together with the columns & data we want to use
+    new agGrid.Grid(eGridDiv, gridOptions);
+});
